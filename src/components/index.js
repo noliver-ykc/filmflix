101cd36a40d5f12b191cbd54c2f2e34d0d18d669
Nicole Olliver <noliver.ykc@gmail.com>
2022-10-17 22:30:52 +0900

    created sidebar navigation

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as MovieInformation } from './MovieInformation/MovieInformation
 export { default as Movies } from './Movies/Movies';
 export { default as NavBar } from './NavBar/NavBar';
 export { default as Profile } from './Profile/Profile';
+export { default as Sidebar } from './Sidebar/Sidebar';
 
 // instead of crowding out components on the app js we can
 // write them all here and import them in one line
